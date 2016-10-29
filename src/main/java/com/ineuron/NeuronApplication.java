84774262355587add7b6fb74bd0216c6e1bbc7f8
xinshuai lin <xinshuai lin>
2016-10-29 13:30:44 +0800

    add angularjs and bootstrap

--- src/main/java/com/ineuron/NeuronApplication.java
+++ src/main/java/com/ineuron/NeuronApplication.java
@@ -2,43 +2,54 @@ package com.ineuron;
 
 import com.ineuron.health.TemplateHealthCheck;
 import com.ineuron.resources.HelloWorldResource;
+import com.ineuron.resources.PersonResource;
+import com.ineuron.resources.UserResource;
+
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
+
 
 /**
- * Hello world!
+ * 
  *
  */
-public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
-    public static void main(String[] args) throws Exception {
-        new HelloWorldApplication().run(args);
-    }
+public class NeuronApplication extends Application<NeuronConfiguration> {
+	public static void main(String[] args) throws Exception {
+		new NeuronApplication().run(args);
+	}
 
-    @Override
-    public String getName() {
-        return "hello-world";
-    }
+	@Override
+	public String getName() {
+		return "I-Neuron";
+	}
 
-    @Override
-    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
-        // nothing to do yet
-    }
+	@Override
+	public void initialize(Bootstrap<NeuronConfiguration> bootstrap) {
+		bootstrap.addBundle(new ViewBundle<NeuronConfiguration>());
+		bootstrap.addBundle(new AssetsBundle("/assets/", "/assets/"));
+	}
 
-    @Override
-    public void run(HelloWorldConfiguration configuration,
-                    Environment environment) {
-    	final HelloWorldResource resource = new HelloWorldResource(
-    	        configuration.getTemplate(),
-    	        configuration.getDefaultName()
-    	    );
-    	
-    	final TemplateHealthCheck healthCheck =
-    	        new TemplateHealthCheck(configuration.getTemplate());
-    	
-    	    environment.healthChecks().register("template", healthCheck);
+	@Override
+	public void run(NeuronConfiguration configuration, Environment environment) throws Exception {
+		
+		final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(),
+				configuration.getDefaultName());
 
-    	    environment.jersey().register(resource);
-    }
+		final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+
+		environment.healthChecks().register("template", healthCheck);
+
+		environment.jersey().register(resource);
+		
+		
+		final PersonResource pResource = new PersonResource();
+		environment.jersey().register(pResource);
+		
+		final UserResource uResource = new UserResource();
+		environment.jersey().register(uResource);
+	}
 
 }
---
+++ src/main/java/com/ineuron/view/PersonView.java
@@ -0,0 +1,16 @@
+package com.ineuron.view;
+
+import io.dropwizard.views.View;
+
+public class PersonView extends View {
+    private final Person person;
+
+    public PersonView(Person person) {
+        super("person.ftl");
+        this.person = person;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+}

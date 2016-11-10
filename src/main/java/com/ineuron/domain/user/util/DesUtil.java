e94cdd32608ddb74ddb04575a7ee84300764a0f6
xinshuai lin <xinshuai lin>
2016-11-11 00:07:01 +0800

    add logging feature

--- src/main/java/com/ineuron/domain/user/util/DesUtil.java
+++ src/main/java/com/ineuron/domain/user/util/DesUtil.java
@@ -2,6 +2,7 @@ package com.ineuron.domain.user.util;
 
 import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Random;
 
 import javax.crypto.Cipher;
@@ -9,8 +10,6 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 public class DesUtil {
 
@@ -38,7 +37,7 @@ public class DesUtil {
 	public static String encrypt(String data) throws Exception {
 		String key = getKey();
 		byte[] bt = encrypt(data.getBytes(), key.getBytes());
-		String strs = new BASE64Encoder().encode(bt);
+		String strs = Base64.getEncoder().encodeToString(bt);
 		return strs;
 	}
 
@@ -58,8 +57,8 @@ public class DesUtil {
 
 		if (data == null)
 			return null;
-		BASE64Decoder decoder = new BASE64Decoder();
-		byte[] buf = decoder.decodeBuffer(data);
+		
+		byte[] buf = Base64.getDecoder().decode(data);
 		byte[] bt = decrypt(buf, key.getBytes());
 		return new String(bt);
 	}

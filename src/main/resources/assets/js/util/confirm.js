5e9f5e563e8bc7a3ed41bd0c9f05977bb31a7ce8
xinshuai lin <xinshuai lin>
2016-11-20 14:40:21 +0800

    enable ng-show for menus

---
+++ src/main/resources/assets/js/util/confirm.js
@@ -0,0 +1,39 @@
+
+ineuronApp.clickok = false;
+
+ineuronApp.confirm = function(title, message, size, $rootScope, $modal){ 
+	var vm = this;
+	var scope = $rootScope.$new();
+	scope.data = {
+			title:title,
+			content:message
+	   }
+	
+	var modalInstance = $modal.open({
+		templateUrl : 'modaltemplate.html',  
+		controller : 'ModalInstanceCtrl',
+		size : size, // default:middle; sm, lg
+		scope:scope,
+		resolve : {
+			body : function(){
+				return ineuronApp.clickok;
+			}
+		}
+	})
+}
+
+
+ineuronApp.controller('ModalInstanceCtrl',function($scope, $modalInstance, $modal, body){
+	$scope.title = $scope.data.title;
+    $scope.content=$scope.data.content;
+
+	$scope.ok = function(){  
+		ineuronApp.clickok=true;
+		$modalInstance.close(ineuronApp.clickok); 
+	};
+	$scope.cancel = function(){
+		ineuronApp.clickok=false;
+		$modalInstance.close(ineuronApp.clickok); 
+	}
+});
+
